66ad2f666172c8a12a6ded2d72e73bcd6e0d0c81
ArxeFD <arsenny.filipov@gmail.com>
2025-10-26 20:51:36 -0600

    Change to Spring Boot because using servlets with Tomcat is a pain in the ass

---
+++ src/main/java/com/arxecommerce/ArxecommerceApp.java
@@ -0,0 +1,11 @@
+package com.arxecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArxecommerceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ArxecommerceApp.class, args);
+    }
+}
